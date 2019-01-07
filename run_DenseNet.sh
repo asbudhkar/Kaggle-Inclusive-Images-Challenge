b4e92ec02cec75a1af4a9360d2d1d16b6ca4707e
asbudhkar <46059569+asbudhkar@users.noreply.github.com>
2019-01-07 11:16:40 +0530

    Update run_DenseNet.sh

--- run_DenseNet.sh
+++ run_DenseNet.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=80GB
 #SBATCH --time=150:00:00
 #SBATCH --output=/scratch/asb862/slurm_%j.out
-#SBATCH --job-name=dense50000_f1
+#SBATCH --job-name=densenet
 #SBATCH --mail-type=END
 #SBATCH --mail-user=asb862@nyu.edu
 python DenseNet_train.py 
